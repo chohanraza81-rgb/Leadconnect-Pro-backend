4aca41c7a53dd33e934e83da751f1ac6f172df0e
chohanraza81-rgb <chohanraza81@gmail.com>
2026-08-06 16:29:54 +0500

    Update settings.js

--- routes/settings.js
+++ routes/settings.js
@@ -1,35 +1,12 @@
-const router = require('express').Router();
-const Setting = require('../models/Setting');
-const { updateConfig } = require('../services/config');
+const mongoose = require('mongoose');
 
-router.get('/', async (req, res) => {
-  try {
-    let settings = await Setting.findOne();
-    res.json(settings || {});
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
+const settingSchema = new mongoose.Schema({
+  gmail: String,
+  appPassword: String,
+  serpApiKey: String,
+  groqApiKey: String,
+  resendApiKey: String,
+  brevoApiKey: String,   // NEW: Brevo API key
 });
 
-router.put('/', async (req, res) => {
-  try {
-    const { gmail, appPassword, serpApiKey, groqApiKey, resendApiKey } = req.body;
-    let settings = await Setting.findOne();
-    if (!settings) {
-      settings = new Setting({ gmail, appPassword, serpApiKey, groqApiKey, resendApiKey });
-    } else {
-      if (gmail !== undefined) settings.gmail = gmail;
-      if (appPassword !== undefined) settings.appPassword = appPassword;
-      if (serpApiKey !== undefined) settings.serpApiKey = serpApiKey;
-      if (groqApiKey !== undefined) settings.groqApiKey = groqApiKey;
-      if (resendApiKey !== undefined) settings.resendApiKey = resendApiKey;
-    }
-    await settings.save();
-    updateConfig({ gmail, appPassword, serpApiKey, groqApiKey, resendApiKey });
-    res.json(settings);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
-});
-
-module.exports = router;
+module.exports = mongoose.model('Setting', settingSchema);
