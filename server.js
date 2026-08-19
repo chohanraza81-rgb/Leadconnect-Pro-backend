777fa4c5fad1edb8bc761e60c0790bddf7aaf4f9
chohanraza81-rgb <chohanraza81@gmail.com>
2026-08-19 15:49:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const { loadSettings } = require('./services/config');
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 loadSettings().then(() => console.log('Settings loaded'));
 
