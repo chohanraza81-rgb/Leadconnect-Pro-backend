300ce8f2ae8e506f63e21783eb834de58449be5b
chohanraza81-rgb <chohanraza81@gmail.com>
2026-08-19 10:49:33 +0500

    Update Setting.js

--- models/Setting.js
+++ models/Setting.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+
 const settingSchema = new mongoose.Schema({
   gmail: String,
   appPassword: String,
   serpApiKey: String,
   groqApiKey: String,
   brevoApiKey: String,
+  scraperApiKey: String,   // NEW
 });
+
 module.exports = mongoose.model('Setting', settingSchema);
